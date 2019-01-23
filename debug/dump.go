e1f71f0d9eac881602d2b769a326d7008a38e16e
Brian W. Wolter <brian@brianwolter.com>
2019-01-22 21:37:39 -0500

    Gofmt

--- debug/dump.go
+++ debug/dump.go
@@ -1,19 +1,19 @@
 package debug
 
 import (
-  "os"
-  "io"
-  "encoding/json"
+	"encoding/json"
+	"io"
+	"os"
 )
 
 func Dump(v ...interface{}) {
-  Dumpf(os.Stdout, v...)
+	Dumpf(os.Stdout, v...)
 }
 
 func Dumpf(w io.Writer, v ...interface{}) {
-  e := json.NewEncoder(w)
-  e.SetIndent("", "  ")
-  for _, x := range v {
-    e.Encode(x)
-  }
+	e := json.NewEncoder(w)
+	e.SetIndent("", "  ")
+	for _, x := range v {
+		e.Encode(x)
+	}
 }
