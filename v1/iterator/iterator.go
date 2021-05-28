ebce53a3a0f986b4acac91da8af33c3f4d87d811
Brian W. Wolter <brian@brianwolter.com>
2021-05-28 16:44:59 -0400

    Iterator

---
+++ v1/iterator/iterator.go
@@ -0,0 +1,59 @@
+package iterator
+
+import (
+	"context"
+	"errors"
+	"sync"
+)
+
+var (
+	ErrClosed    = errors.New("Closed")
+	ErrCancelled = errors.New("Cancelled")
+)
+
+type Result struct {
+	Elem  interface{}
+	Error error
+}
+
+type Iterator struct {
+	cxt    context.Context
+	data   chan Result
+	closer sync.Once
+}
+
+func New(cxt context.Context, buf int) *Iterator {
+	return &Iterator{
+		cxt:  cxt,
+		data: make(chan Result, buf),
+	}
+}
+
+func (t Iterator) Write(r Result) error {
+	select {
+	case <-t.cxt.Done():
+		return ErrCancelled
+	case t.data <- r:
+		return nil
+	}
+}
+
+func (t Iterator) Close() error {
+	t.closer.Do(func() {
+		close(t.data)
+	})
+	return nil
+}
+
+func (t Iterator) Next() (interface{}, error) {
+	select {
+	case <-t.cxt.Done():
+		return nil, ErrCancelled
+	case v, ok := <-t.data:
+		if ok {
+			return v.Elem, v.Error
+		} else {
+			return nil, ErrClosed
+		}
+	}
+}
---
+++ v1/iterator/iterator_test.go
@@ -0,0 +1,68 @@
+package iterator
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIterator(t *testing.T) {
+	var err error
+
+	n := 10
+	it := New(context.Background(), n)
+	for i := 0; i < n; i++ {
+		err = it.Write(Result{Elem: i})
+		assert.Nil(t, err, fmt.Sprint(err))
+	}
+
+	err = it.Close()
+	assert.Nil(t, err, fmt.Sprint(err))
+
+	for i := 0; ; i++ {
+		val, err := it.Next()
+		if err == ErrClosed {
+			break
+		} else {
+			assert.Nil(t, err, fmt.Sprint(err))
+		}
+		assert.Equal(t, i, val.(int))
+	}
+
+}
+
+func TestCancellation(t *testing.T) {
+	cxt, cancel := context.WithCancel(context.Background())
+	var err error
+
+	n := 10
+	it := New(cxt, n)
+	for i := 0; i < n; i++ {
+		err = it.Write(Result{Elem: i})
+		assert.Nil(t, err, fmt.Sprint(err))
+	}
+
+	cancel()
+
+	// this is deterministic because the channel buffer is full, so
+	// cancellation is the only case that is ready when Write() is
+	// called
+	err = it.Write(Result{Elem: 100})
+	assert.Equal(t, ErrCancelled, err)
+
+	// this is not deterministic, so we have to process up to N before
+	// we're guaranteed to get the cancellation error
+	var found bool
+	for i := 0; i < n; i++ {
+		_, err = it.Next()
+		if err != nil {
+			assert.Equal(t, ErrCancelled, err)
+			found = true
+			break
+		}
+	}
+	assert.Equal(t, true, found)
+
+}
