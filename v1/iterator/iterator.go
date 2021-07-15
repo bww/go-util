7dbdbbdfa225a066d8bc4629f7c9789aff632f57
Brian W. Wolter <brian@brianwolter.com>
2021-07-15 15:56:52 -0400

    Avoid possible write on closed

--- v1/iterator/iterator.go
+++ v1/iterator/iterator.go
@@ -17,20 +17,26 @@ type Result struct {
 }
 
 type Iterator struct {
+	mx     sync.RWMutex
 	cxt    context.Context
 	data   chan Result
-	closer *sync.Once
+	closed bool
 }
 
-func New(cxt context.Context, buf int) Iterator {
-	return Iterator{
-		cxt:    cxt,
-		data:   make(chan Result, buf),
-		closer: &sync.Once{},
+func New(cxt context.Context, buf int) *Iterator {
+	return &Iterator{
+		mx:   sync.RWMutex{},
+		cxt:  cxt,
+		data: make(chan Result, buf),
 	}
 }
 
-func (t Iterator) Write(r Result) error {
+func (t *Iterator) Write(r Result) error {
+	t.mx.RLock()
+	defer t.mx.RUnlock()
+	if t.closed {
+		return ErrClosed
+	}
 	select {
 	case <-t.cxt.Done():
 		return ErrCancelled
@@ -39,14 +45,18 @@ func (t Iterator) Write(r Result) error {
 	}
 }
 
-func (t Iterator) Close() error {
-	t.closer.Do(func() {
+func (t *Iterator) Close() error {
+	t.mx.Lock()
+	defer t.mx.Unlock()
+	if !t.closed {
 		close(t.data)
-	})
+		t.closed = true
+	}
 	return nil
 }
 
-func (t Iterator) Next() (interface{}, error) {
+func (t *Iterator) Next() (interface{}, error) {
+	// don't need to check closed	for read
 	select {
 	case <-t.cxt.Done():
 		return nil, ErrCancelled
