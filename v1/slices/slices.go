56ae9ec5286ccd0d58d8d8f6fffb80429e9f54fe
Brian W. Wolter <brian@brianwolter.com>
2022-10-25 16:33:01 -0400

    Adding slices.Map

--- v1/slices/slices.go
+++ v1/slices/slices.go
@@ -8,3 +8,13 @@ func Apply[T any](s []T, f func(T) T) []T {
 	}
 	return s
 }
+
+// Map every element in an input slice to a countepart output
+// element by applying the specified function.
+func Map[X, Y any](s []X, f func(X) Y) []Y {
+	r := make([]Y, len(s))
+	for i, e := range s {
+		r[i] = f(e)
+	}
+	return r
+}
--- v1/slices/slices_test.go
+++ v1/slices/slices_test.go
@@ -24,3 +24,23 @@ func TestApply(t *testing.T) {
 		assert.Equal(t, e.Output, Apply(e.Input, e.Func))
 	}
 }
+
+func TestMap(t *testing.T) {
+	tests := []struct {
+		Input  []rune
+		Output []int
+		Func   func(rune) int
+	}{
+		{
+			[]rune{'a', 'b', 'c'},
+			[]int{0, 1, 2},
+			func(c rune) int { // map a lowercase latin rune to it's alphabetic ordinal
+				return int(c) - int('a')
+			},
+		},
+	}
+
+	for _, e := range tests {
+		assert.Equal(t, e.Output, Map(e.Input, e.Func))
+	}
+}
