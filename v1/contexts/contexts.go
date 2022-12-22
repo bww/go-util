43babce831940374f46e00a9428e6dfa5308e641
Brian W. Wolter <brianwolter@gmail.com>
2022-12-22 08:47:07 -0500

    Remove duplicative cancellation error

--- v1/contexts/contexts.go
+++ v1/contexts/contexts.go
@@ -2,19 +2,15 @@ package contexts
 
 import (
 	"context"
-	"errors"
 )
-
-var ErrCancelled = errors.New("Cancelled by context")
 
 // Continue is a convenience wrapper around a common context
 // cancelation check. It is intended to be used when processing
 // a loop that may be canceled; like so:
 //
-//     for contexts.Continue(cxt) {
-//       // continue processing something...
-//     }
-//
+//	for contexts.Continue(cxt) {
+//	  // continue processing something...
+//	}
 func Continue(cxt context.Context) bool {
 	select {
 	case <-cxt.Done():
@@ -27,12 +23,12 @@ func Continue(cxt context.Context) bool {
 // Continue is a convenience wrapper around a common context
 // cancelation check. It is essentially the inverse of Continue:
 //
-//     for {
-//       if contexts.Done(cxt) {
-//         break
-//       }
-//       // otherwise...
-//     }
+//	for {
+//	  if contexts.Done(cxt) {
+//	    break
+//	  }
+//	  // otherwise...
+//	}
 func Done(cxt context.Context) bool {
 	select {
 	case <-cxt.Done():
