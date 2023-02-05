82dd878991c9243216ae0d69905b83cb5f4dd9ba
Brian W. Wolter <brianwolter@gmail.com>
2023-02-05 10:15:23 -0600

    Parse template config

--- v1/text/template/template.go
+++ v1/text/template/template.go
@@ -5,6 +5,26 @@ import (
 	"text/template"
 )
 
+type Config struct {
+	Funcs template.FuncMap
+}
+
+func (c Config) WithOptions(opts []Option) Config {
+	for _, opt := range opts {
+		c = opt(c)
+	}
+	return c
+}
+
+type Option func(Config) Config
+
+func WithFuncs(f template.FuncMap) Option {
+	return func(conf Config) Config {
+		conf.Funcs = f
+		return conf
+	}
+}
+
 func Exec(f string, d interface{}) ([]byte, error) {
 	t, err := Parse(f)
 	if err != nil {
@@ -17,8 +37,16 @@ type Template struct {
 	*template.Template
 }
 
-func Parse(f string) (*Template, error) {
-	t, err := template.New("_").Parse(f)
+func Parse(f string, opts ...Option) (*Template, error) {
+	return ParseWithConfig(f, Config{}.WithOptions(opts))
+}
+
+func ParseWithConfig(f string, conf Config) (*Template, error) {
+	t := template.New("_")
+	if conf.Funcs != nil {
+		t.Funcs(conf.Funcs)
+	}
+	t, err := t.Parse(f)
 	if err != nil {
 		return nil, err
 	}
--- v1/text/template/template_test.go
+++ v1/text/template/template_test.go
@@ -3,9 +3,14 @@ package template
 import (
 	"fmt"
 	"testing"
+	"text/template"
 
 	"github.com/stretchr/testify/assert"
 )
+
+var funcs = template.FuncMap{
+	"hello": func() string { return "hello!" },
+}
 
 func TestTemplateOneshot(t *testing.T) {
 	tests := []struct {
@@ -40,6 +45,32 @@ func TestTemplateOneshot(t *testing.T) {
 	}
 	for _, e := range tests {
 		res, err := Exec(e.Tmpl, e.Data)
+		if e.Error {
+			fmt.Println("***", err)
+			assert.NotNil(t, err, fmt.Sprint(err))
+		} else if assert.Nil(t, err, fmt.Sprint(err)) {
+			fmt.Println("-->", string(e.Expect))
+			assert.Equal(t, e.Expect, res)
+		}
+	}
+}
+
+func TestTemplateWithFuncs(t *testing.T) {
+	tests := []struct {
+		Tmpl   string
+		Data   interface{}
+		Expect []byte
+		Error  bool
+	}{
+		{
+			Tmpl:   "Oh, {{ hello }}",
+			Expect: []byte("Oh, hello!"),
+		},
+	}
+	for _, e := range tests {
+		tmpl, err := Parse(e.Tmpl, WithFuncs(funcs))
+		assert.NoError(t, err)
+		res, err := tmpl.Exec(e.Data)
 		if e.Error {
 			fmt.Println("***", err)
 			assert.NotNil(t, err, fmt.Sprint(err))
