2cd36ffe3ee1198f17cf977a50d47ff307c6933c
Brian W. Wolter <brianwolter@gmail.com>
2023-10-13 17:22:13 -0400

    ...

--- v1/ext/ext.go
+++ v1/ext/ext.go
@@ -12,3 +12,18 @@ func Choose[T any](expr bool, ift T, iff T) T {
 		return iff
 	}
 }
+
+// Coalesce returns the first non-zero value in the provided arguments. If
+// no argument is non-zero, the zero value is returned.
+//
+//	Coalesce("", "", "hello", "") // "hello"
+//	Coalesce(nil, ptr, nil) // ptr
+func Coalesce[T comparable](v ...T) T {
+	var z T
+	for _, e := range v {
+		if e != z {
+			return e
+		}
+	}
+	return z
+}
--- v1/ext/ext_test.go
+++ v1/ext/ext_test.go
@@ -13,3 +13,9 @@ func TestChoose(t *testing.T) {
 	assert.Equal(t, 1, Choose(true, 1, 2))
 	assert.Equal(t, 2, Choose(false, 1, 2))
 }
+
+func TestCoalesce(t *testing.T) {
+	i := 100
+	assert.Equal(t, "hello", Coalesce("", "hello", ""))
+	assert.Equal(t, &i, Coalesce(nil, &i, nil))
+}
