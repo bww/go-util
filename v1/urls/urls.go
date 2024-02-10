fd633b8122ebea66fdbfba0126ca16672675c08d
Brian W. Wolter <brianwolter@gmail.com>
2024-02-10 10:48:04 -0800

    ...

--- v1/urls/urls.go
+++ v1/urls/urls.go
@@ -6,6 +6,29 @@ import (
 	"path"
 )
 
+// Join a base URL together with path components, similar to the
+// functionality of path.Join.
+func Join(b string, c ...interface{}) string {
+	if len(c) < 1 {
+		return b
+	}
+	s := make([]string, len(c))
+	for i, e := range c {
+		s[i] = fmt.Sprint(e)
+	}
+	p := path.Join(s...)
+	for i, e := range p {
+		if e != '/' {
+			p = p[i:]
+			break
+		}
+	}
+	if l := len(b); l < 1 || b[l-1] != '/' {
+		b = b + "/"
+	}
+	return b + p
+}
+
 // Merge the specified queury parameters into the provided URL. Parameters
 // with existing keys are added, not replaced.
 func MergeQuery(s string, p ...url.Values) (string, error) {
@@ -41,29 +64,6 @@ func MergeQueryOpts(s string, p url.Values, opts ...MergeOption) (string, error)
 	}
 	u.RawQuery = MergeValues(u.Query(), p, opts...).Encode()
 	return u.String(), nil
-}
-
-// Join a base URL together with path components, similar to the
-// functionality of path.Join.
-func Join(b string, c ...interface{}) string {
-	if len(c) < 1 {
-		return b
-	}
-	s := make([]string, len(c))
-	for i, e := range c {
-		s[i] = fmt.Sprint(e)
-	}
-	p := path.Join(s...)
-	for i, e := range p {
-		if e != '/' {
-			p = p[i:]
-			break
-		}
-	}
-	if l := len(b); l < 1 || b[l-1] != '/' {
-		b = b + "/"
-	}
-	return b + p
 }
 
 // Return a file URL for the provided path
