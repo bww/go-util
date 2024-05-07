da764a5e96a1d5ce74fe4b77c49ad117fd1c2b1a
Brian W. Wolter <brianwolter@gmail.com>
2024-05-07 20:02:52 +0200

    Adding maps.Merge, maps.Merged

--- v1/maps/maps.go
+++ v1/maps/maps.go
@@ -8,3 +8,35 @@ func Copy[K comparable, V any](m map[K]V) map[K]V {
 	}
 	return d
 }
+
+// Merge merges the contents of several maps together into the first provided
+// map, which is also returned. Each successive instance of a key found in any
+// map overwrites a previously encountered instance. Provide the maps in
+// inverse order of priority.
+//
+// If no maps are provided as input, nil is returned, not an empty map.
+func Merge[K comparable, V any](s ...map[K]V) map[K]V {
+	l := len(s)
+	if l == 0 {
+		return nil
+	} else if l == 1 {
+		return s[0]
+	}
+	d := s[0] // first map is also the output
+	for i := 1; i < l; i++ {
+		for k, v := range s[i] {
+			d[k] = v
+		}
+	}
+	return d
+}
+
+// Merged has the same behavior as Merge(), except that the maps are merged
+// into a newly allocated map, not one of the parameters.
+func Merged[K comparable, V any](s ...map[K]V) map[K]V {
+	if len(s) > 0 {
+		return Merge(append([]map[K]V{make(map[K]V)}, s...)...)
+	} else {
+		return nil
+	}
+}
--- v1/maps/maps_test.go
+++ v1/maps/maps_test.go
@@ -21,3 +21,62 @@ func TestCopy(t *testing.T) {
 		assert.Equal(t, e.Output, Copy(e.Input))
 	}
 }
+
+func TestMergeMerged(t *testing.T) {
+	tests := []struct {
+		Input  []map[int]string
+		Output map[int]string
+	}{
+		{
+			nil,
+			nil,
+		},
+		{
+			[]map[int]string{},
+			nil,
+		},
+		{
+			[]map[int]string{
+				map[int]string{1: "One", 2: "Two"},
+			},
+			map[int]string{1: "One", 2: "Two"},
+		},
+		{
+			[]map[int]string{
+				map[int]string{1: "One", 2: "Two"},
+				map[int]string{1: "One", 2: "Two"},
+			},
+			map[int]string{1: "One", 2: "Two"},
+		},
+		{
+			[]map[int]string{
+				map[int]string{1: "One", 2: "Two"},
+				map[int]string{3: "Three"},
+			},
+			map[int]string{1: "One", 2: "Two", 3: "Three"},
+		},
+		{
+			[]map[int]string{
+				map[int]string{1: "One", 2: "Two"},
+				map[int]string{1: "Three"},
+			},
+			map[int]string{1: "Three", 2: "Two"},
+		},
+		{
+			[]map[int]string{
+				map[int]string{1: "One", 2: "Two"},
+				map[int]string{1: "Three"},
+				map[int]string{1: "Four"},
+			},
+			map[int]string{1: "Four", 2: "Two"},
+		},
+	}
+
+	for _, e := range tests {
+		assert.Equal(t, e.Output, Merged(e.Input...))
+		assert.Equal(t, e.Output, Merge(e.Input...))
+		if len(e.Input) > 0 {
+			assert.Equal(t, e.Output, e.Input[0]) // input was mutated
+		}
+	}
+}
